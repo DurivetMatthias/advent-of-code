e95dbb05e30f5e8a3ce5639ec84c4d9197a5cd23
Matthias Durivet <matthias.durivet@ml6.eu>
2023-12-13 09:52:05 +0100

    Solve day 12 by iteratively simplifying the possibilites

--- 2023/12/main.js
+++ 2023/12/main.js
@@ -1,5 +1,5 @@
 const { readFile } = require("../../common/io.js")
-const { } = require("../../common/util.js")
+const { zip } = require("../../common/util.js")
 
 const parsed = readFile(__dirname)
     .split("\n")
@@ -12,103 +12,99 @@ const springs = parsed.map((line) => {
     const unfoldedArrangement = (arrangement + "?").repeat(5).slice(0, -1)
     const unfoldedGroups = (groups + ",").repeat(5).slice(0, -1)
     return {
-        arrangement: unfoldedArrangement,
-        groups: unfoldedGroups.split(",").map(Number),
+        // arrangement: unfoldedArrangement,
+        // groups: unfoldedGroups.split(",").map(Number),
+        arrangement,
+        groups: groups.split(",").map(Number),
     }
 })
 console.log('springs:', springs)
 
-const permutations = (list, maxLen) => {
-    // Copy initial values as arrays
-    var perm = list.map(function (val) {
-        return [val]
-    })
-    // Our permutation generator
-    var generate = function (perm, maxLen, currLen) {
-        // Reached desired length
-        if (currLen === maxLen) {
-            return perm
+const withCache = (fn) => {
+    const cache = new Map()
+    return (...args) => {
+        const key = JSON.stringify(args)
+        if (cache.has(key)) {
+            return cache.get(key)
         }
-        // For each existing permutation
-        for (var i = 0, len = perm.length; i < len; i++) {
-            var currPerm = perm.shift()
-            // Create new permutation
-            for (var k = 0; k < list.length; k++) {
-                perm.push(currPerm.concat(list[k]))
+        const result = fn(...args)
+        cache.set(key, result)
+        return result
+    }
+}
+
+const getGroups = (arrangement) => {
+    const earlyGroups = [...arrangement.matchAll(/(\#)\1*/g)]
+        .map(regexMatch => regexMatch[0].length)
+    return earlyGroups
+}
+
+const charsLessThanOrEqualTo = (arrangement, group) => {
+    return (arrangement.match(/#/g) || []).length <= group.reduce((a, b) => a + b)
+}
+
+const amountOfGroupsLessThanOrEqualTo = (arrangement, groups) => {
+    const ifAllHash = arrangement.replaceAll("?", "#")
+    const ifAllHashGroups = getGroups(ifAllHash)
+    return ifAllHashGroups.length <= groups.length
+}
+
+const firstGroupLTE = (arrangement, groups) => {
+    const firstGroup = getGroups(arrangement)[0]
+    return firstGroup <= groups[0]
+}
+
+const ENABLE_DEBUG = true
+const debug = (...args) => {
+    if (ENABLE_DEBUG) console.log(...args)
+}
+
+const isStillPossible = (arrangement, groups) => {
+    const a = firstGroupLTE(arrangement, groups)
+    debug('a:', a)
+    return a
+}
+
+const replaceAt = (string, index, replacement) => {
+    return string.substring(0, index) + replacement + string.substring(index + 1)
+}
+
+const simplify = ({ arrangement, groups }) => {
+    let previousArrangement = arrangement
+    let newArrangement = null
+    while (previousArrangement !== newArrangement) {
+        newArrangement = previousArrangement
+        for (let index = 0; index < newArrangement.length; index++) {
+            if (newArrangement[index] === "?") {
+                {
+                    debug("==================")
+                    const ifHash = replaceAt(newArrangement, index, "#")
+                    const possibleIfHash = isStillPossible(ifHash, groups)
+                    debug('ifHash:', ifHash)
+                    debug('possibleIfHash:', possibleIfHash)
+                    debug("==================")
+                    // If a hash doesn't work, it must be a dot
+                    // Simplify the arrangement and keep going
+                    if (!possibleIfHash) newArrangement = replaceAt(newArrangement, index, ".")
+                }
             }
         }
-        // Recurse
-        return generate(perm, maxLen, currLen + 1)
-    }
-    // Start with size 1 because of initial values
-    return generate(perm, maxLen, 1)
-}
-
-const hasValidGroups = (arrangement, groups) => {
-    const brokenSpringRegex = /(\#)\1*/g
-    const brokenGroups = [...arrangement.matchAll(brokenSpringRegex)]
-        .map(regexMatch => regexMatch[0].length)
-    return brokenGroups.join("-") === groups.join("-")
-}
-
-// const test = hasValidGroups("#.#", [1, 1])
-// console.log('test:', test)
-
-const earlyHasValidGroups = (partialArrangement, groups) => {
-    const firstQuestionMark = partialArrangement.indexOf("?")
-    let filledArrangement = partialArrangement.substring(0, firstQuestionMark)
-    while (filledArrangement.at(-1) === "#") {
-        filledArrangement = filledArrangement.slice(0, -1)
-    }
-    const brokenSpringRegex = /(\#)\1*/g
-    const brokenGroups = [...filledArrangement.matchAll(brokenSpringRegex)]
-        .map(regexMatch => regexMatch[0].length)
-    return brokenGroups.join("-") === groups.slice(0, brokenGroups.length).join("-")
-}
-const testTwo = earlyHasValidGroups("#.#.???", [1, 1, 3])
-console.log('testTwo:', testTwo)
-
-const permutationMap = {}
-const largestUnknown = "?????????????????"
-for (let i = 1; i <= largestUnknown.length; i++) {
-    permutationMap[i] = permutations([".", "#"], i).map((permutation) => permutation.join(""))
-}
-console.log("calculated permutation map")
-
-const possibilities = springs.map(({ arrangement, groups }) => {
-    const flatOptions = [arrangement]
-    const unknownRegex = /(\?)\1*/g
-    while (flatOptions.some(option => option.includes("?"))) {
-        // console.log("=====================================")
-        // console.log('flatOptions:', flatOptions)
-        // console.log("=====================================")
-        option = flatOptions.shift()
-        const match = option.match(unknownRegex)[0]
-        const length = match.length
-        const permutations = permutationMap[length]
-        permutations.forEach((permutation) => {
-            const newOption = option.replace(match, permutation)
-            // console.log("=====================================")
-            // console.log('newOption:', newOption)
-            // console.log('groups:', groups)
-            // console.log('earlyHasValidGroups(newOption, groups):', earlyHasValidGroups(newOption, groups))
-            if (earlyHasValidGroups(newOption, groups)) {
-                flatOptions.push(newOption)
+        for (let index = 0; index < newArrangement.length; index++) {
+            if (newArrangement[index] === "?") {
+                debug("==================")
+                const ifDot = replaceAt(newArrangement, index, ".")
+                const possibleIfDot = isStillPossible(ifDot, groups)
+                debug('ifDot:', ifDot)
+                debug('possibleIfDot:', possibleIfDot)
+                debug("==================")
+                // If a dot doesn't work, it must be a hash
+                // Simplify the arrangement and keep going
+                if (!possibleIfDot) newArrangement = replaceAt(newArrangement, index, "#")
             }
-            // console.log("=====================================")
-        })
+            return { arrangement, simplified: newArrangement, groups }
+        }
     }
-    return flatOptions
+}
 
-})
-// console.log('possibilities:', possibilities)
-
-const validPossibilities = possibilities.map((possibilities, i) => {
-    return possibilities.filter((possibility) => hasValidGroups(possibility, springs[i].groups))
-})
-// console.log('validPossibilities:', validPossibilities)
-
-const sumOfPossibilities = validPossibilities
-    .map((possibilities) => possibilities.length)
-    .reduce((a, b) => a + b)
-console.log('sumOfPossibilities:', sumOfPossibilities)
+const simplifiedSprings = springs.map(simplify)
+console.log('simplifiedSprings:', simplifiedSprings)
